fe01cc2aa7d54b4e2e64687b92ef09d3c4609cd0
kiyohiro kawamura <kyorohiro@kiyohiro-no-Mac-mini.local>
2016-02-13 09:04:39 +0900

    add ios code

---
+++ src/ios/TinyBeacon.h
@@ -0,0 +1,18 @@
+//
+//  TinyBeacon.h
+//  HelloCordova
+//
+//  Created by kiyohiro kawamura on 2016/02/13.
+//
+//
+
+#ifndef TinyBeacon_h
+#define TinyBeacon_h
+
+#import <Cordova/CDV.h>
+
+
+@interface TinyBeacon : CDVPlugin
+
+@end
+#endif /* TinyBeacon_h */
---
+++ src/ios/TinyBeacon.m
@@ -0,0 +1,15 @@
+//
+//  TinyBeacon.m
+//  HelloCordova
+//
+//  Created by kiyohiro kawamura on 2016/02/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "TinyBeacon.h"
+
+@implementation  TinyBeacon
+
+
+@end
