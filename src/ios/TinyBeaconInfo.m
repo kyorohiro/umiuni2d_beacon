e8156fc03f79155e513435a1193280c96ade83e7
kiyohiro kawamura <kyorohiro@kiyohiro-no-Mac-mini.local>
2016-02-14 10:14:54 +0900

    ..

--- src/ios/TinyBeaconInfo.m
+++ src/ios/TinyBeaconInfo.m
@@ -9,57 +9,6 @@
 #import <Foundation/Foundation.h>
 #import "TinyBeacon.h"
 
-@implementation TinyBeacinInfoList
-- (id) init {
-    self.beaconInfos = [[NSMutableArray alloc] init];
-    return self;
-}
-
-- (TinyBeaconInfo*) getTinyBeaconInfoFromBeaconRegion: (CLBeaconRegion*) region {
-    for(TinyBeaconInfo *i in self.beaconInfos) {
-        if([i.region isEqual:region] == YES) {
-            return i;
-        }
-    }
-    return nil;
-}
-
-- (TinyBeaconInfo*) getTinyBeaconInfoFromBeaconFromUUID: (NSString*) uuid major:(NSNumber*)major minor:(NSNumber*)minor {
-    for(TinyBeaconInfo *i in self.beaconInfos) {
-        if([[i.region.proximityUUID UUIDString] isEqual:uuid] == NO) {
-            continue;
-        }
-        if([[i.region major] intValue] != [major intValue]) {
-            continue;
-        }
-        if([[i.region minor] intValue] != [minor intValue]) {
-            continue;
-        }
-        return i;
-    }
-    return nil;
-}
-
-- (TinyBeaconInfo*) putTinyBeaconInfo:(NSString*) uuid major:(NSNumber*)major minor:(NSNumber*)minor {
-    TinyBeaconInfo *info =[self getTinyBeaconInfoFromBeaconFromUUID:uuid major:nil minor:nil];
-    if(info == nil) {
-        info =[[TinyBeaconInfo alloc] initWithUUID:uuid];
-        [self.beaconInfos addObject:info];
-    }
-    return info;
-}
-
-- (TinyBeaconInfo*) putTinyBeaconInfoFromBeaconRegion: (CLBeaconRegion*) region {
-    TinyBeaconInfo *info = [self getTinyBeaconInfoFromBeaconRegion:(CLBeaconRegion*)region];
-    if(info == nil){
-        info = [[TinyBeaconInfo alloc] initWithBeaconRegion:(CLBeaconRegion*)region];
-        [self.beaconInfos addObject:info];
-    }
-    return info;
-}
-
-@end
-
 @implementation TinyBeaconInfo
 - (id)initWithUUID:(NSString*)uuid {
     NSUUID * uuidObj = [[NSUUID alloc] initWithUUIDString:uuid];
