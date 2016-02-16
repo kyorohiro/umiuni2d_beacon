e4052bc0b673d0b2fea86c822eaedefd036de0aa
kiyohiro kawamura <kyorohiro@kiyohiro-no-Mac-mini.local>
2016-02-17 01:16:43 +0900

    ..

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cordova-plugin-hello
+
+# install
+cordova plugin add https://github.com/kyorohiro/cordova-plugin-tinybeacon.git
+
+
--- dart_example/web/main.dart
+++ dart_example/web/main.dart
@@ -1,8 +1,39 @@
 // Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'dart:html';
+import 'dart:html' as html;
+import 'package:tinybeacon/tinybeacon.dart';
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  TinyBeacon beacon = new TinyBeacon();
+  {
+    html.InputElement startLescanButton = new html.InputElement(type: "button");
+    startLescanButton.value = "startlescan";
+    startLescanButton.onClick.listen((html.MouseEvent e) {
+      print("click startlescan");
+    });
+    html.document.body.children.add(startLescanButton);
+  }
+  {
+    html.InputElement stopLescanButton = new html.InputElement(type: "button");
+    stopLescanButton.value = "stoplescan";
+    stopLescanButton.onClick.listen((html.MouseEvent e) {
+      print("click stoplescan");
+    });
+    html.document.body.children.add(stopLescanButton);
+  }
+
+  {
+    html.InputElement stopLescanButton = new html.InputElement(type: "button");
+    stopLescanButton.value = "requestPermissions";
+    stopLescanButton.onClick.listen((html.MouseEvent e) {
+      print("click requestPermissions");
+      try {
+        beacon.requestPermissions();
+      } catch(e) {
+        print("failed to requestPermissions");
+      }
+    });
+    html.document.body.children.add(stopLescanButton);
+  }
 }
