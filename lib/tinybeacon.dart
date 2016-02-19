db44eae3a640736858e2aa1bb11637db25b0989b
kiyohiro kawamura <kyorohiro@kiyohiro-no-Mac-mini.local>
2016-02-20 02:59:03 +0900

    ..

--- lib/tinybeacon.dart
+++ lib/tinybeacon.dart
@@ -36,38 +36,41 @@ class TinyCordova {
 }
 
 enum TinyBeaconRequestFlag {
-  WHEN_IN_USE,//= "foreground_only";
-  ALWAYS,//_REQUEST = "background"
+  WHEN_IN_USE, //= "foreground_only";
+  ALWAYS, //_REQUEST = "background"
 }
-enum TinyBeaconScanFlag {
-  LOW,
-  NORMAL,
-  HIGH
-}
+enum TinyBeaconScanFlag { LOW, NORMAL, HIGH }
 
 class TinyBeaconScanInfo {
-    String uuid;
-    int major;
-    int minor;
-    TinyBeaconScanInfo(this.uuid, {this.major:null, this.minor:null}){;}
-    String normalizeUUID() {
-      if(uuid.contains("-")) {
-        return uuid;
-      } else {
-        return
-        "${uuid.substring(0,8)}-${uuid.substring(8,12)}"+
-        "-${uuid.substring(12,16)}-${uuid.substring(16,20)}"+
-        "-${uuid.substring(20)}";
-      }
+  String uuid;
+  int major;
+  int minor;
+  TinyBeaconScanInfo(this.uuid, {this.major: null, this.minor: null}) {
+    ;
+  }
+  String normalizeUUID() {
+    if (uuid.contains("-")) {
+      return uuid;
+    } else {
+      return "${uuid.substring(0,8)}-${uuid.substring(8,12)}" + "-${uuid.substring(12,16)}-${uuid.substring(16,20)}" + "-${uuid.substring(20)}";
     }
+  }
 
-    String toJSONString() {
-      Map r = {};
-      r["uuid"] = normalizeUUID();
-      r["major"] = major;
-      r["minor"] = minor;
-      return JSON.encode(r);
-    }
+  String toJSONString() {
+    Map r = {};
+    r["uuid"] = normalizeUUID();
+    r["major"] = major;
+    r["minor"] = minor;
+    return JSON.encode(r);
+  }
+
+  Map toMap() {
+    Map r = {};
+    r["uuid"] = normalizeUUID();
+    r["major"] = major;
+    r["minor"] = minor;
+    return r;
+  }
 }
 
 class TinyBeacon {
@@ -76,23 +79,38 @@ class TinyBeacon {
   //
   //
   // android only : empty beacons is scan all beacon.
-  startLescan(List<TinyBeaconScanInfo> beacons,{flag:TinyBeaconScanFlag.NORMAL}) async {
-    List args = [];
-    args.add(flag);
-    args.addAll(beacons);
-    return await cordova.exec("TinyBeacon", "startLescan", args);
+  startLescan(List<TinyBeaconScanInfo> beacons, {flag: TinyBeaconScanFlag.NORMAL}) async {
+    Map args = {};
+    switch (flag) {
+      case TinyBeaconScanFlag.LOW:
+        args["power"]= "low";
+        break;
+      case TinyBeaconScanFlag.HIGH:
+        args["power"]= "high";
+        break;
+      case TinyBeaconScanFlag.NORMAL:
+      default:
+        args["power"]= "normal";
+        break;
+    }
+    List beaconArgs = [];
+    for(TinyBeaconScanInfo s in beacons) {
+      beaconArgs.add(s.toMap());
+    }
+    args["beacons"] = beaconArgs;
+    return await cordova.exec("TinyBeacon", "startLescan", [JSON.encode(args)]);
   }
 
   stopLescan() async {
     return await cordova.exec("TinyBeacon", "stopLescan", []);
   }
 
-  requestPermissions({TinyBeaconRequestFlag flag:TinyBeaconRequestFlag.WHEN_IN_USE}) async {
+  requestPermissions({TinyBeaconRequestFlag flag: TinyBeaconRequestFlag.WHEN_IN_USE}) async {
     String flagForCordova;
-    if(flag == TinyBeaconRequestFlag.WHEN_IN_USE) {
-      flagForCordova  = "when_in_use";
+    if (flag == TinyBeaconRequestFlag.WHEN_IN_USE) {
+      flagForCordova = "when_in_use";
     } else {
-      flagForCordova  = "always";
+      flagForCordova = "always";
     }
     return await cordova.exec("TinyBeacon", "requestPermissions", [flagForCordova]);
   }
