1ff56bb8867a78276b4c1fdd103cfb18800f7156
kiyohiro kawamura <kyorohiro@kiyohiro-no-Mac-mini.local>
2016-02-20 08:50:44 +0900

    ..

--- lib/tinybeacon_mock.dart
+++ lib/tinybeacon_mock.dart
@@ -4,20 +4,33 @@ import 'dart:async';
 import 'tinybeacon.dart';
 
 class TinyBeaconMock extends TinyBeacon {
+  bool isScanning = false;
+  TinyBeaconFoundResultMock beaconResult = new TinyBeaconFoundResultMock();
+
   //
   // android only : empty beacons is scan all beacon.
   startLescan(List<TinyBeaconScanInfo> beacons, {flag: TinyBeaconScanFlag.NORMAL}) async {
+    isScanning = true;
   }
 
   stopLescan() async {
+    isScanning = false;
   }
 
   requestPermissions({TinyBeaconRequestFlag flag: TinyBeaconRequestFlag.WHEN_IN_USE}) async {
   }
 
   Future<TinyBeaconFoundResult> getFoundBeacon() async {
+    return beaconResult;
   }
 
   clearFoundedBeacon() async {
   }
+
+}
+
+class TinyBeaconFoundResultMock extends TinyBeaconFoundResult {
+  int timePerSec = 0;
+
+  List<TinyBeaconFoundBeacon> beacons = [];
 }
