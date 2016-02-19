df5d4a248e29dd6024c917603a99a20d02bfb1d5
kiyohiro kawamura <kyorohiro@kiyohiro-no-Mac-mini.local>
2016-02-20 06:10:40 +0900

    ..

---
+++ lib/tinybeacon_mock.dart
@@ -0,0 +1,25 @@
+library umiuni2d_beacon_mock;
+
+import 'dart:async';
+import 'dart:convert';
+import 'tinycordova.dart';
+import 'tinybeacon.dart';
+
+class TinyBeaconMock extends TinyBeacon {
+  //
+  // android only : empty beacons is scan all beacon.
+  startLescan(List<TinyBeaconScanInfo> beacons, {flag: TinyBeaconScanFlag.NORMAL}) async {
+  }
+
+  stopLescan() async {
+  }
+
+  requestPermissions({TinyBeaconRequestFlag flag: TinyBeaconRequestFlag.WHEN_IN_USE}) async {
+  }
+
+  Future<String> getFoundBeacon() async {
+  }
+
+  clearFoundedBeacon() async {
+  }
+}
